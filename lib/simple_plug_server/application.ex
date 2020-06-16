0aa10659e7fff96dffbf0e4233a63e712034c767
evanilukhin <evanilukhin@gmail.com>
2020-06-16 08:55:45 +0300

    Init commit

---
+++ lib/simple_plug_server/application.ex
@@ -0,0 +1,19 @@
+defmodule SimplePlugServer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: SimplePlugServer.Worker.start_link(arg)
+      # {SimplePlugServer.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: SimplePlugServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
